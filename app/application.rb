e2958b72c1851cce6c2da5c9b82915f9dcb9c90e
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-29 02:40:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,8 @@ class Application
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
+    resp.write "#{num_1}\n"
+    
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else resp.write "You Lose"
